39930f292d9ece4dd1d53e9467f02f06bfe203ce
Maximka Lomakin <korcuganov71@gmail.com>
2024-11-15 20:51:17 +0300

    Animations for main-block

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -43,3 +43,7 @@ var slider = tns({
 });
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+Tu.tScroll({
+  't-element': '.t-animated',
+  't-duration': 0.9
+});
