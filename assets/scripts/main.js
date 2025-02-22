4f6a83d398532b7dd456845e6c0c862572fcc9cf
Maximka <korcuganov71@gmail.com>
2025-02-22 22:45:18 +0300

    Some fixes v2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,15 +13,16 @@ setTimeout(() => {
 
 var modal = document.getElementById('bookingModal');
   modal.addEventListener('shown.bs.modal', function() {
-      var iframe = modal.querySelector('iframe');
-      if (iframe) {
-          iframe.setAttribute('src', 'https://koalendar.com/e/appliance-repair-appointment-with-ek-global?embed=true');
-      }
+    var iframe = document.createElement('iframe');
+    iframe.className = 'iframe-form';
+    iframe.src = 'https://koalendar.com/e/appliance-repair-appointment-with-ek-global?embed=true';
+    iframe.id = 'bookingIframe';
+    modal.querySelector('.modal-body').appendChild(iframe);
   });
   modal.addEventListener('hide.bs.modal', function() {
-      var iframe = modal.querySelector('iframe');
-      if (iframe) {
-          iframe.removeAttribute('src');
-      }
+    var iframe = modal.querySelector('#bookingIframe');
+    if (iframe) {
+        iframe.remove();
+    }
   });
 });
